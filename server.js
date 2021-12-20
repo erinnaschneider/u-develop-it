6bdb99fdc11b46a29efc7fc023bb7b9d27413d1e
Erinn Schneider <erinnaschneider@gmail.com>
2021-12-20 18:33:14 -0500

    connected database with express.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mysql username,
+        user: 'root',
+        // your mysql password
+        password: 'gajt666blko',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
